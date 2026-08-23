981c22f86f582a404ce839965147bcc9d1958060
vak-rashu <rashu.cloud11@gmail.com>
2026-08-23 11:49:52 +0000

    more on haskell

--- hello.hs
+++ hello.hs
@@ -1,2 +1,11 @@
 main :: IO()
 main = putStrLn "Hello, Haskell!"
+
+-- define type of x
+x, y :: Int
+x = 3
+-- how does this work with no error?
+y = y+1
+
+-- cannot define x again because it is defined as 3 and cannot be changed again
+-- x = 4
