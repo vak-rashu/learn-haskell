d11bc424d30bc1a0c42dad1346640eed94b00757
vak-rashu <rashu.cloud11@gmail.com>
2026-08-24 11:28:50 +0000

    small steps: equational reasoning

--- hello.hs
+++ hello.hs
@@ -1,11 +1,20 @@
+-- import 
+
 main :: IO()
-main = putStrLn "Hello, Haskell!"
+main = do 
+    putStrLn "Hello, Haskell!"
+    print x
+    print x
+    print x
 
 -- define type of x
 x, y :: Int
 x = 3
 -- how does this work with no error?
 y = y+1
+-- f :: IO ()
+-- f = do
+--     print x
 
 -- cannot define x again because it is defined as 3 and cannot be changed again
 -- x = 4
